9618217c710b8f866c1a28b973ecc9ccf869e993
Milena <90552287+MilenaYermolaieva@users.noreply.github.com>
2021-12-15 11:49:05 +0200

    Create 1.c

---
+++ 1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(void)
+// Вивести на екран рисунок із літер (хатинка)
+{
+    printf("       Ж \n");
+    printf("      ЖЖЖ \n");
+    printf("     ЖЖЖЖЖ \n");
+    printf("    ЖЖЖЖЖЖЖ \n");
+    printf("     НН НН   \n");
+    printf("     ZZZZZ   \n");
+}
