45c1ae07a8aecefc21e099e7a8f1199bb1f785f4
Milena <90552287+MilenaYermolaieva@users.noreply.github.com>
2021-12-15 11:55:28 +0200

    Create 4.c

---
+++ 4.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main()
+  //Увести п’ять чисел і знайти найбільше з них.
+{
+    int m,n,l,k,r;
+    printf("Введіть 5 чисел: \n");
+    scanf("%i%i%i%i%i", &m,&n,&l, &k, &r);
+    if ( m > n && m > l && m > k && m > r)
+    {
+        printf("Найбільше число %i\n", m);
+    }
+    else if (n > m && n > l && n > k && n > r)
+    {
+        printf("Найбільше число %i \n", n );
+    }
+    else if (l > m && l > m && l > k && l > r)
+    {
+        printf("Найбільше число %i \n", l );
+    }
+    else if (k > m && k > m && k > l && k > r)
+    {
+        printf("Найбільше число %i \n", k );
+    }
+    else
+    {
+        printf("Найбільше число %i \n", r );
+    }
+}
