70bf95cfa60502869c4ed638623cd73009cb0943
Milena <90552287+MilenaYermolaieva@users.noreply.github.com>
2021-12-15 12:06:55 +0200

    Update 13.c

--- 13.c
+++ 13.c
@@ -1,36 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
-//Написати процедуру, яка ставить на початок масиву всі парні елементи, а у кінець – всі непарні
-int sort( int N[], int n)
-{
-    int pP = 0, pN = n - 1 ;
-    int M[n];
-    for(int i = 0; i < n; i++){
-        if(N[i] % 2 == 0){
-            M[pP++] = N[i];
-        }else{
-            M[pN--] = N[i];
-        }
-    }
-    for(int i = 0; i < n; i++){
-        N[i]=M[i];
-    }
-    return 0;
-}
+//Заповнити масив випадковими числами і виділити в інший масив усі числа, які зустрічаються більше одного разу
 int main()
 {
-    int r[12];
+    int r[7], res[7], point = 0;
     srandom(time(NULL));
     printf("Початковий масив:\n");
-    for(int i = 0; i < 12; i++){
-        r[i] = random()%21 - 10;
-        printf("%i ", r[i]);
+    for(int i = 0; i < 7; i++){
+        r[i] = random()%101;
+        printf("%i ",r[i]);
     }
-    sort(r, 12);
     printf("\nРезультат:\n");
-    for(int i = 0; i < 12; i++){
-        printf("%i ", r[i]);
+    
+    for(int i = 0; i < 7; i++){
+        if(r[i]==-1) continue;
+        int count = 0;
+        for(int q = i+1; q < 7; q++){
+            if(r[q]==-1) continue;
+            if(r[i] == r[q] ){
+                r[q] = -1;
+                count++;
+            }
+        }
+        if(count == 0){
+            r[i] = -1;
+        }
+    }
+    for(int i = 0; i < 7; i++){
+        if(r[i]!=-1){
+            res[point++] = r[i];
+            printf("%i ", r[i]);
+        }
     }
 }
